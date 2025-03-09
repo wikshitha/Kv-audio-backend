ada59f5af59b7adebcdac67cf4a190a497670af4
Wikshitha Umindu <uminduwikshitha@gmail.com>
2025-03-09 11:28:38 +0530

    Getorder controll done👐

--- controllers/orderControl.js
+++ controllers/orderControl.js
@@ -76,3 +76,56 @@ export async function createOrder(req,res) {
         res.status(500).json({message:"Failed to Create Order"})
     }
 }
+
+export async function getQuote(req,res) {
+    const data = req.body
+
+    const orderInfo = {
+        orderedItems : []
+    }
+    let  oneDayCost = 0
+
+    for(let i=0;i<data.orderedItems.length;i++){
+        try {
+            const product = await Product.findOne({key : data.orderedItems[i].key})
+            if(product == null) {
+                res.status(404).json({message:"Product with key "+data.orderedItems[i].key+" not found"})
+                return
+            }
+            if(product.availability == false) {
+                res.status(404).json({message:"Product with key "+data.orderedItems[i].key+" is not available"})
+                return
+            }
+
+            orderInfo.orderedItems.push({
+                product : {
+                    key : product.key,
+                    name : product.name,
+                    image : product.images[0],
+                    price : product.price
+                },
+                quantity : data.orderedItems[i].qty
+            })
+
+            oneDayCost += product.price * data.orderedItems[i].qty
+
+        }catch (error) {
+            res.status(500).json({message:"Order Creation Failed"})
+            return
+        }
+    }
+
+    orderInfo.days = data.days
+    orderInfo.startingDate = data.startingDate
+    orderInfo.endingDate = data.endingDate
+    orderInfo.totalAmount = oneDayCost * data.days
+
+    try{
+        res.json({
+            message:"Order quetation",
+            order : orderInfo.totalAmount
+        })
+    }catch (error) {
+        res.status(500).json({message:"Failed to Create Order"})
+    }
+}
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createOrder } from "../controllers/orderControl.js";
+import { createOrder, getQuote } from "../controllers/orderControl.js";
+
 
 const orderRouter = express.Router();
 
 orderRouter.post("/",createOrder)
+orderRouter.post("/quote",getQuote)
 
 export default orderRouter
